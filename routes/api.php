675675fa5fc383d2b98eebd1c2e1bb6d2af43104
Juan <kaenel10@gmail.com>
2020-07-03 15:26:14 -0300

    Creando Modelos -a

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//el Resource y ApiResource se diferencia en que en el apiResource no incluye las rutas edit y create
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix'=>'products'],function (){
+    Route::apiResource('/{product}/reviews','ReviewController');
+});
+
