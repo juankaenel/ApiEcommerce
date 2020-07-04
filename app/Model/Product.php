f185ae4724376bfe6e5b0a8651e21d09d0c91f83
Juan <kaenel10@gmail.com>
2020-07-04 00:21:29 -0300

    Creando la relación entre Product y Review

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+        /*un producto puede tener muchos reviews*/
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,9 +3,13 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    //
+    public function product(){
+        /*un review pertenece a un producto*/
+        return $this->belongsTo(Product::class);
+    }
 }
 
