baa38bf898633fbc0238686acc95dcb3df17ee6e
Juan <kaenel10@gmail.com>
2020-07-04 01:46:20 -0300

    Modificando detalles de Product

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,15 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+
+            //variables extra
+            'totalPrice' => round((1-($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ] //El rating solo se mostrará para aquellos productos q tenga por lo menos un review. El rating está dado por la suma de estrellas sobre la cantidad de reviews. Viene a ser el promedio de estrellas
         ];
     }
 }
