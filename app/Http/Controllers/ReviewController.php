8a30328fceb87ffd8332381f0fdb5440b1ea90d2
Juan <kaenel10@gmail.com>
2020-07-04 20:56:52 -0300

    Review Resource & Paginación de Product

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ReviewResource;
+use App\Model\Product;
 use App\Model\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
 
-    public function index()
+    public function index(Product $product)
     {
-        //
+         return ReviewResource::collection($product->reviews);
     }
 
 
--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Resources\Product;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+   /*Información de los productos en general (index) */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[
+            'name'=>$this->name,
+            'totalPrice' => round((1-($this->discount/100)) * $this->price,2),
+            'discount' => $this->discount,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href' => [ //si quiero ver el artículo específico le paso la ruta
+                'link' => route('products.show',$this->id)
+            ],
+        ];
     }
 }
